31d450ca8eca54f6f2471c53acde6c645146d2e4
Jokast38 <115721772+Jokast38@users.noreply.github.com>
2025-10-07 22:27:16 +0200

    Update tab bar active tint color in TabLayout to match design specifications

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function TabLayout() {
     return (
-        <Tabs screenOptions={{tabBarActiveTintColor: 'blue', headerShown: false}}>
+        <Tabs screenOptions={{tabBarActiveTintColor: '#3E8E68', headerShown: false}}>
             <Tabs.Screen name="photos" options={{tabBarLabel: "Photos", tabBarIcon: ({color,size}) => <Ionicons name="images" color={color} size={size} />}}/>
             <Tabs.Screen name="map" options={{tabBarLabel: "Carte", tabBarIcon: ({color,size}) => <Ionicons name="map" color={color} size={size} />}}/>
             <Tabs.Screen name="camera" options={{tabBarLabel: "Camera", tabBarIcon: ({color,size}) => <Ionicons name="camera" color={color} size={size} />}}/>
