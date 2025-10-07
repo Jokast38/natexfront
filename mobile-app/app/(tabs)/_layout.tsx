d09a27e8956cce4ea1ca0e47fda2993c5b7614ec
Jokast38 <115721772+Jokast38@users.noreply.github.com>
2025-10-07 20:02:54 +0200

    Add tab icons to TabLayout for improved navigation experience

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,13 +1,15 @@
 import {Tabs} from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
 
 export default function TabLayout() {
     return (
         <Tabs screenOptions={{tabBarActiveTintColor: 'blue', headerShown: false}}>
-            <Tabs.Screen name="photos" options={{tabBarLabel: "Photos"}}/>
-            <Tabs.Screen name="map" options={{tabBarLabel: "Carte"}}/>
-            <Tabs.Screen name="camera" options={{tabBarLabel: "Camera"}}/>
-            <Tabs.Screen name="calendar" options={{tabBarLabel: "Calendrier"}}/>
-            <Tabs.Screen name="profile" options={{tabBarLabel: "Profil"}}/>
+            <Tabs.Screen name="photos" options={{tabBarLabel: "Photos", tabBarIcon: ({color,size}) => <Ionicons name="images" color={color} size={size} />}}/>
+            <Tabs.Screen name="map" options={{tabBarLabel: "Carte", tabBarIcon: ({color,size}) => <Ionicons name="map" color={color} size={size} />}}/>
+            <Tabs.Screen name="camera" options={{tabBarLabel: "Camera", tabBarIcon: ({color,size}) => <Ionicons name="camera" color={color} size={size} />}}/>
+            <Tabs.Screen name="calendar" options={{tabBarLabel: "Calendrier", tabBarIcon: ({color,size}) => <Ionicons name="calendar" color={color} size={size} />}}/>
+            <Tabs.Screen name="profile" options={{tabBarLabel: "Profil", tabBarIcon: ({color,size}) => <Ionicons name="person" color={color} size={size} />}}/>
         </Tabs>
     );
 }
